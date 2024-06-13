d83b11662ef9c4ea67147f3b6fdde485a2d1db96
Kseniia Matveeva <ksum.miha@gmail.com>
2024-06-13 09:52:05 -0300

    Update py scripts

--- preprocessing.py
+++ preprocessing.py
@@ -123,3 +123,80 @@ def qc_and_preprocess_to_pca(adata: AnnData, output_dir: str = None) -> AnnData:
     sc.tl.pca(adata)
 
     return adata
+
+
+def aggregate_and_filter(
+    adata,
+    cell_identity=None,
+    donor_key="bulk_sample",
+    condition_key="dea_labels",
+    cell_identity_key=None,
+    obs_to_keep=[],  # which additional metadata to keep, e.g. gender, age, etc.
+    replicates_per_patient=1,
+    min_cells_per_donor=30
+    ):
+    """
+    Creates an AnnData object with one pseudo-replicate for each donor for a specified 
+    subpopulation from the original single-cell AnnData object. 
+    Also it filters out donors that have fewer than `min_cells_per_donor' cells 
+    for the specified population.
+
+    By changing the `replicates_per_patient` parameter, 
+    several (n) pseudo-replicates can be created for each sample; 
+    cells are then split into n subsets of roughly equal sizes.
+    """
+    # subset adata to the given cell identity
+    if cell_identity:
+        adata_cell_pop = adata[adata.obs[cell_identity_key] == cell_identity].copy()
+    adata_cell_pop = adata
+    
+    # check which donors to keep according to the number of cells specified with NUM_OF_CELL_PER_DONOR
+    size_by_donor = adata_cell_pop.obs.groupby([donor_key]).size()
+    donors_to_drop = [
+        donor
+        for donor in size_by_donor.index
+        if size_by_donor[donor] <= min_cells_per_donor
+    ]
+    if len(donors_to_drop) > 0:
+        print("Dropping the following samples:")
+        print(donors_to_drop)
+    df = pd.DataFrame(columns=[*adata_cell_pop.var_names, *obs_to_keep])
+
+    adata_cell_pop.obs[donor_key] = adata_cell_pop.obs[donor_key].astype("category")
+    
+    for i, donor in enumerate(donors := adata_cell_pop.obs[donor_key].cat.categories):
+        print(f"\tProcessing donor {i+1} out of {len(donors)}...", end="\r")
+        
+        if donor not in donors_to_drop:
+            adata_donor = adata_cell_pop[adata_cell_pop.obs[donor_key] == donor]
+            
+            # create replicates for each donor
+            indices = list(adata_donor.obs_names)
+            random.shuffle(indices)
+            indices = np.array_split(np.array(indices), replicates_per_patient)
+            for i, rep_idx in enumerate(indices):
+                adata_replicate = adata_donor[rep_idx]
+                
+                # specify how to aggregate: sum gene expression 
+                # for each gene for each donor and also keep the condition information
+                agg_dict = {gene: "sum" for gene in adata_replicate.var_names}
+                for obs in obs_to_keep:
+                    agg_dict[obs] = "first"
+                    
+                # create a df with all genes, donor and condition info
+                df_donor = pd.DataFrame(adata_replicate.X.A)
+                df_donor.index = adata_replicate.obs_names
+                df_donor.columns = adata_replicate.var_names
+                df_donor = df_donor.join(adata_replicate.obs[obs_to_keep])
+                
+                # aggregate
+                df_donor = df_donor.groupby(donor_key).agg(agg_dict)
+                df_donor[donor_key] = donor
+                df.loc[f"{donor}_{i}"] = df_donor.loc[donor]
+    print("\n")
+    
+    # create AnnData object from the df
+    adata_cell_pop = sc.AnnData(
+        df[adata_cell_pop.var_names], obs=df.drop(columns=adata_cell_pop.var_names)
+    )
+    return adata_cell_pop
